d9b75a9cac31e36423006f5955e99e6e70fb3a06
d.i.ie <d.i.ie@tinkoff.ru>
2020-12-09 18:28:29 +0300

    пускаем егора 7

--- src/main/java/com/system/accounting/security/HeaderCorsFilter.java
+++ src/main/java/com/system/accounting/security/HeaderCorsFilter.java
@@ -15,7 +15,7 @@ public class HeaderCorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
                                     final FilterChain filterChain) throws ServletException, IOException {
-        response.addHeader("Access-Control-Allow-Origin", "*");
+//        response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD, OPTIONS");
         response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
         response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization");
