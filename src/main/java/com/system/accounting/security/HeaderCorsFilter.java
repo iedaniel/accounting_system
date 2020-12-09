80cf15ef375ec5879c0346e5c92737177cbb0963
d.i.ie <d.i.ie@tinkoff.ru>
2020-12-09 17:42:06 +0300

    пускаем егора 4

--- src/main/java/com/system/accounting/security/HeaderCorsFilter.java
+++ src/main/java/com/system/accounting/security/HeaderCorsFilter.java
@@ -39,12 +39,12 @@ public class HeaderCorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
                                     final FilterChain filterChain) throws ServletException, IOException {
-        response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD, OPTIONS");
         response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
         response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization");
         response.addHeader("Access-Control-Allow-Credentials", "true");
-        response.addIntHeader("Access-Control-Max-Age", 10);
+        response.addIntHeader("Access-Control-Max-Age", 600);
         filterChain.doFilter(request, response);
     }
 }
