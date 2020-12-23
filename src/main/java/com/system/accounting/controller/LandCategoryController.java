2593f02f528cf1fbb92657fc673d432c5adcd359
d.i.ie <d.i.ie@tinkoff.ru>
2020-12-23 06:13:50 +0300

    докинул в свагер описания

--- src/main/java/com/system/accounting/controller/LandCategoryController.java
+++ src/main/java/com/system/accounting/controller/LandCategoryController.java
@@ -4,6 +4,8 @@ import com.system.accounting.model.dto.BaseResponse;
 import com.system.accounting.model.dto.land_category.LandCategoriesResponse;
 import com.system.accounting.model.dto.land_category.LandCategoryCreateRequest;
 import com.system.accounting.service.land_category.LandCategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,12 +17,14 @@ public class LandCategoryController {
     private final LandCategoryService landCategoryService;
 
     @PostMapping("/create")
+    @ApiOperation("Создать земельную категорию")
     public BaseResponse<?> create(@RequestBody LandCategoryCreateRequest request) {
         landCategoryService.create(request);
         return new BaseResponse<>();
     }
 
     @GetMapping
+    @ApiOperation("Отобразить список земельных категорий")
     public BaseResponse<LandCategoriesResponse> getCategories() {
         return new BaseResponse<>(landCategoryService.getCategories());
     }
